d1b0c1f4bf0af951ea5fdcf74d838c210d928ce4
Obada-Yahya <obadayahya.an@gmail.com>
2023-09-04 18:32:23 +0300

    adding the Bot entity and their subclasses

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-
 namespace RealTimeWeather
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ SunBot.cs
@@ -0,0 +1,9 @@
+﻿namespace RealTimeWeather;
+
+public class SunBot : Bot
+{
+    public SunBot(string message) : base(message)
+    {
+        
+    }
+}
