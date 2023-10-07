557cb6eb00afc67782fc2a95e11be96f7fe35250
Obada-Yahya <obadayahya.an@gmail.com>
2023-10-07 20:49:00 +0300

    Add unit tests for JsonvFormatValidator class

---
+++ RealTimeWeatherTests/JsonFormatValidatorTests.cs
@@ -0,0 +1,29 @@
+using Real_time_weather;
+using RealTimeWeatherTests.TestData;
+
+namespace RealTimeWeatherTests;
+
+public class JsonFormatValidatorTests
+{
+    [Theory]
+    [MemberData(nameof(JsonFormatValidatorTestData.ValidateFormatValidTestData),
+        MemberType = typeof(JsonFormatValidatorTestData))]
+    public void ValidateFormat_ForValidJson_ReturnTrue(string actualData)
+    {
+        var validateFormat = new JsonFormatValidator();
+        var keys = KeyTypeUtil.GetWeatherInfoKeys();
+        var actual = validateFormat.ValidateFormat(actualData, keys);
+        Assert.True(actual);
+    }
+
+    [Theory]
+    [MemberData(nameof(JsonFormatValidatorTestData.ValidateFormatInValidTestData),
+        MemberType = typeof(JsonFormatValidatorTestData))]
+    public void ValidateFormat_forInvalidJson_ReturnFalse(string actualData)
+    {
+        var validateFormat = new JsonFormatValidator();
+        var keys = KeyTypeUtil.GetWeatherInfoKeys();
+        var actual = validateFormat.ValidateFormat(actualData, keys);
+        Assert.False(actual);
+    }
+}
